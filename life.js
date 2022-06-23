fa893464a47c1904a1e3ab809c152fa4345080a1
chuksmic <michealchegwe@gmail.com>
2022-06-23 14:31:20 +0100

    first commit

---
+++ life.js
@@ -0,0 +1,9 @@
+alert (" my name is micheal chegwe")
+alert (" i am tall with height 44.7")
+alert (" i am from nigeria")
+
+
+console .log(myname);
+console .log(hieght);
+console .log (mycountry)
+
